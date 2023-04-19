0e5f66e7a6001b32bff374b64e4dfc5ae65cbeec
Iceblok <ycoskun0318@gmail.com>
2023-04-20 01:49:35 +0200

    tst

--- Opdracht_0.py
+++ Opdracht_0.py
@@ -5,8 +5,10 @@ import itertools
 Maak een functie die twee vectoren bij elkaar optelt. De vectoren zijn Numpy arrays.
 """
 
+
 class DimensionError(Exception):
     pass
+
 
 def vector_addition(u: np.ndarray, v: np.ndarray) -> np.ndarray:
     summed_arrays = u.copy()
@@ -19,8 +21,8 @@ def vector_addition(u: np.ndarray, v: np.ndarray) -> np.ndarray:
         return summed_arrays
 
 
-# print(vector_addition([4, 5, 6], [0., 0., 0.]))
-print(vector_addition([4, 5, 6], [0., 0.]))
+print(vector_addition([4, 5, 6], [0., 0., 0.]))
+# print(vector_addition([4, 5, 6], [0., 0.]))
 
 
 
