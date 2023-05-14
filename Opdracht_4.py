cb3ae3b4632b04263ed8e24ceba692cd48067e06
Iceblok <ycoskun0318@gmail.com>
2023-05-14 15:29:21 +0200

    opdrachten gecomment

--- Opdracht_4.py
+++ Opdracht_4.py
@@ -43,8 +43,104 @@ def matrix_vector(M: np.ndarray, v: np.ndarray) -> np.ndarray:
 
         return result_matrix_vector
 
+#
+# v = np.array([7, -7])
+# M = np.array(([2, 0], [17, 17], [2, 14]))
+#
+# print(matrix_vector(M, v))
 
-v = np.array([7, -7])
-M = np.array(([2, 0], [17, 17], [2, 14]))
+def matrix_product(M: np.ndarray, N: np.ndarray) -> np.ndarray:
+    """
+    Function returns matrix-matrix multiplication.
+    A matrix and a vector multiplication is also possible.
+    Example;
+    :param M: np.ndarray, np.array(([6, 5, 4], [9, 8, 7]))
+    :param N: np.ndarray, np.array(([4, 0], [4, 5], [8, 3]))
+    :return: result_matrix_product: np.ndarray,
+    """
+    if M.shape[1] != N.shape[0]:
+        raise DimensionError("Vectors niet hetzelfde formaat")
 
-print(matrix_vector(M, v))
+    elif M.shape[1] == N.shape[0]:
+        if len(N.shape) == 1:
+            return matrix_vector(M, N)
+
+        elif len(N.shape) > 1:
+            result_matrix_product = np.zeros((M.shape[0], N.shape[1]))
+            for i in range(N.shape[1]):
+                tijdelijk = N[:, i]
+                a = matrix_vector(M, tijdelijk)
+                for x in range(M.shape[0]):
+                    result_matrix_product[x][i] = a[x]
+            return result_matrix_product
+
+
+# m = np.array(([6, 5, 4], [9, 8, 7]))
+# n = np.array(([4, 0], [4, 5], [8, 3]))
+#
+#
+# print(matrix_product(m, n))
+#
+# np.array(([76.,  37.], [124.,  61.]))
+
+import json
+
+
+def read_network(filename: str) -> np.ndarray:
+    """
+    Function reads the layers from the JSON file, and creates matrices for the different layers.
+    If this is more than 1, the layers are multiplied with each other.
+    :param filename: str, given JSON-file
+    :return: result: np.ndarray, A matrix of multiplied matrices
+    """
+    # Network file
+    f_network = open(filename, "r")
+    data = json.loads(f_network.read())
+
+    for layer in data:
+
+        size_in = int(data[layer]["size_in"])
+        size_out = int(data[layer]["size_out"])
+
+        matrix = np.zeros((size_in, size_out))
+
+        for index, column in enumerate(data[layer]["weights"]):
+
+            for i in range(size_out):
+                if str(i + 1) not in data[layer]["weights"][column]:
+                    data[layer]["weights"][column][str(i + 1)] = 0
+
+                matrix[index, i] = data[layer]["weights"][column][str(i + 1)]
+
+        data[layer]["matrix"] = matrix.T
+
+    # Set correct the correct size for output array
+    last_layer = list(data.keys())[-1]
+    result = data[last_layer]["matrix"]
+
+    layer_list = list(data.keys())
+    layer_list_reversed = layer_list[::-1]
+
+    if len(layer_list_reversed) > 1:
+        layer_list_reversed.pop(0)
+
+        for i in range(len(layer_list_reversed)):
+            next_layer_matrix = data[layer_list_reversed[i]]["matrix"]
+            result = matrix_product(result, next_layer_matrix)
+
+    return result
+
+
+def run_network(filename: str, input_vector: np.ndarray) -> np.ndarray:
+    """
+    Function uses read_network() and matrix_vector(),
+    to apply the JSON file to the given input_vector.
+    :param filename: str, given JSON-file
+    :param input_vector: np.ndarray, given Vector
+    :return: result: np.ndarray, JSON-file matrices multiplied * given Vector
+    """
+    file_matrix = read_network(filename)
+
+    result = matrix_vector(file_matrix, input_vector)
+
+    return result
